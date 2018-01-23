a130671ac318a547bd1d3d12b2bbdf00962d0b7f
leepuppychow <leepuppychow@gmail.com>
2018-01-22 17:09:03 -0700

    Merchant index endpoint done

---
+++ spec/factories/merchants.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :merchant do
+    name "Joe"
+    created_at DateTime.now
+    updated_at DateTime.now
+  end
+end
---
+++ spec/requests/api/v1/merchants/merchants_index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it "shows an index of all merchants" do
+    create_list(:merchant, 5)
+
+    get '/api/v1/merchants.json'
+
+    expect(response).to be_success
+
+    merchants = JSON.parse(response.body)
+    expect(merchants.count).to eq 5
+  end
+end
