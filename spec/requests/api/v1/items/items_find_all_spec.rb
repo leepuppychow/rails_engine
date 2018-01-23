930e91c9ed5333470c0db9ab50a540cfbdfdd57c
BLaurenB <blbillington1@gmail.com>
2018-01-23 09:57:03 -0700

    wip

---
+++ spec/requests/api/v1/items/items_find_all_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "User searches for all items by parameters" do
+
+  it "returns a list of valid items by name" do
+
+    create(:item, name:"Item1")
+    create(:item, name:"Item1")
+    create_list(:item, 3)
+
+    get "/api/v1/items/find_all?name=${Item.first.name}"
+
+    served_items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(served_items.count).to eq(2)
+
+  end
+end
