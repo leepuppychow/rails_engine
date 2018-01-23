cbdb97d77a6eaefc8746f92729a90bccaa7b1ea3
leepuppychow <leepuppychow@gmail.com>
2018-01-22 17:35:33 -0700

    Merchant show endpoint test passing

---
+++ spec/requests/api/v1/merchants/merchants_show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it "can see a specific merchant based on its ID" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}.json"
+
+    merchant = JSON.parse(response.body)
+    expect(response).to be_success
+    expect(merchant["id"]).to eq id
+  end
+end
