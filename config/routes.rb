ca22096c83c1f36c79e5187fc9c1052b21d05f3c
leepuppychow <leepuppychow@gmail.com>
2018-01-22 22:26:05 -0700

    Merchant random endpoint working

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/merchants/find', to: "merchants/search#show"
       get '/merchants/find_all', to: "merchants/search#index"
+      get '/merchants/random', to: "merchants/random#show"
       resources :merchants, only: [:index, :show]
     end
   end
