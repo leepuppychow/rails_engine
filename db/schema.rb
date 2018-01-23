b593ebecdb475143bcb24fa16238c8e323c5e9f8
BLaurenB <blbillington1@gmail.com>
2018-01-22 17:52:08 -0700

    tests, model, relationships for items

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123002226) do
+ActiveRecord::Schema.define(version: 20180123003449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "citext"
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
@@ -35,6 +36,15 @@ ActiveRecord::Schema.define(version: 20180123002226) do
     t.integer "customer_id"
     t.integer "merchant_id"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.citext "name"
+    t.citext "description"
+    t.integer "unit_price"
+    t.integer "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
