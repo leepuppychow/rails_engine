bb7e1e48427cec4a21753944932f32f0e532eae2
leepuppychow <leepuppychow@gmail.com>
2018-01-23 13:02:45 -0700

    Customer index endpoint working

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
         get '/find_all', to: "search#index"
         get '/random', to: "random#show"
       end
-      
       resources :merchants, only: [:index, :show]
+
+      resources :customers, only: [:index]
 
 
       namespace :items do
