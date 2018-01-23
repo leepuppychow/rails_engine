9123b03162d0f0cad03a9b3d0d80af04eb0c8009
BLaurenB <blbillington1@gmail.com>
2018-01-23 07:18:08 -0700

    tests working for invoices

--- spec/requests/api/v1/invoices/invoices_show_spec.rb
+++ spec/requests/api/v1/invoices/invoices_show_spec.rb
@@ -1,16 +1,27 @@
 require 'rails_helper'
 
 describe "Invoice Show API" do
-  it "returns a list of all invoices" do
-    create_list(:invoice, 3)
-    invoice = invoice.last
+  it "returns a single invoice" do
+
+    merchant_1 = Merchant.create(name: "Merchant1", created_at: Time.now, updated_at: Time.now)
+    merchant_2 = Merchant.create(name: "Merchant2", created_at: Time.now, updated_at: Time.now)
+    merchant_3 = Merchant.create(name: "Merchant3", created_at: Time.now, updated_at: Time.now)
+    customer_1 = Customer.create(first_name: "FN1",last_name: "LN1", created_at: Time.now, updated_at: Time.now)
+    customer_2 = Customer.create(first_name: "FN2",last_name: "LN2", created_at: Time.now, updated_at: Time.now)
+    customer_3 = Customer.create(first_name: "FN3",last_name: "LN3", created_at: Time.now, updated_at: Time.now)
+
+    Invoice.create(status: "paid", merchant_id: merchant_1.id, customer_id: customer_1.id,created_at: Time.now, updated_at: Time.now)
+    Invoice.create(status: "paid", merchant_id: merchant_2.id, customer_id: customer_2.id,created_at: Time.now, updated_at: Time.now)
+    Invoice.create(status: "paid", merchant_id: merchant_3.id, customer_id: customer_3.id,created_at: Time.now, updated_at: Time.now)
+
+    invoice = Invoice.last
 
     get "/api/v1/invoices/#{invoice.id}"
 
     served_invoice = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-    expect(served_invoice[:name]).to eq(invoice.name)
+    expect(served_invoice[:merchant_id]).to eq(invoice.merchant_id)
 
   end
 
--- spec/requests/api/v1/items/items_show_spec.rb
+++ spec/requests/api/v1/items/items_show_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe "Item Show API" do
-  it "returns a list of all items" do
+  it "returns a single item" do
     create_list(:item, 3)
     item = Item.last
 
     get "/api/v1/items/#{item.id}"
 
     served_item = JSON.parse(response.body, symbolize_names: true)
-# binding.pry
+binding.pry
     expect(response).to be_successful
     expect(served_item[:name]).to eq(item.name)
 
