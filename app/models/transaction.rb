4b03585897df8ae19c59323baff0ec64c97406e5
leepuppychow <leepuppychow@gmail.com>
2018-01-23 16:36:55 -0700

    Added associations to transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,6 +6,10 @@ class Transaction < ApplicationRecord
             :updated_at, presence: true
 
   belongs_to :invoice
+  has_one :customer, through: :invoice
+  has_one :merchant, through: :invoice
+  has_many :invoice_items, through: :invoice
+  
   scope :successful_transactions, -> {where(result: "success")}
 
 
