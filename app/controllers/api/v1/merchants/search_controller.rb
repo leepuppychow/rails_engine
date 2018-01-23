0beb191b8212f8cb7559595130a3e1c3a8368519
leepuppychow <leepuppychow@gmail.com>
2018-01-23 12:40:13 -0700

    Refactored search controller

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -2,17 +2,10 @@ class Api::V1::Merchants::SearchController < ApplicationController
 
   def show
     render json: Merchant.find_by(search_params) if search_params
-
-    # render json: Merchant.find_by(name: params[:name]) if params[:name]
-    # render json: Merchant.find_by(created_at: params[:created_at]) if params[:created_at]
-    # render json: Merchant.find_by(updated_at: params[:updated_at]) if params[:updated_at]
   end
 
   def index
     render json: Merchant.where(search_params) if search_params
-    # render json: Merchant.where(name: params[:name]) if params[:name]
-    # render json: Merchant.where(created_at: params[:created_at]) if params[:created_at]
-    # render json: Merchant.where(updated_at: params[:updated_at]) if params[:updated_at]
   end
 
   private
@@ -22,6 +15,6 @@ class Api::V1::Merchants::SearchController < ApplicationController
       #could create a Sanitize.new PORO to process the currency
       # params[:created_at] = DateTime.new(params[:created_at]) if params[:created_at]
       # params[:updated_at] = DateTime.new(params[:updated_at]) if params[:updated_at]
-      params.permit(:name, :created_at, :updated_at)
+      params.permit(:id, :name, :created_at, :updated_at)
     end
 end
