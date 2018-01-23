56c8bf2adce0cd286893ccfad8f2aa82166e509f
BLaurenB <blbillington1@gmail.com>
2018-01-22 21:07:18 -0700

    writing tests for Items

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :item do
-    
+    sequence(:name)  {|n| "Name#{n}" }
+    sequence(:description)  {|n| "Description for Item Name#{n}" }
+    unit_price 100
+    merchant
+    created_at Time.now
+    updated_at Time.now
   end
 end
