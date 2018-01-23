a7b5b0141215025ede1201f28d66c12d94768a37
BLaurenB <blbillington1@gmail.com>
2018-01-23 08:39:19 -0700

    wip. Pausing to fetch Lee's work

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :invoice_item do
-    
+    quantity 1
+    unit_price 100
+    item
+    invoice
+
   end
 end
--- spec/requests/api/v1/invoice_items/invoice_items_show_spec.rb
+++ spec/requests/api/v1/invoice_items/invoice_items_show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe "Invoice Index API" do
-  it "returns a list of all invoices" do
+describe "Invoice Show API" do
+  it "returns a single invoice" do
 
     merchant_1 = Merchant.create(name: "Merchant1", created_at: Time.now, updated_at: Time.now)
     merchant_2 = Merchant.create(name: "Merchant2", created_at: Time.now, updated_at: Time.now)
@@ -10,16 +10,18 @@ describe "Invoice Index API" do
     customer_2 = Customer.create(first_name: "FN2",last_name: "LN2", created_at: Time.now, updated_at: Time.now)
     customer_3 = Customer.create(first_name: "FN3",last_name: "LN3", created_at: Time.now, updated_at: Time.now)
 
-    Invoice.create(status: "paid", merchant: merchant_1, customer: customer_1, created_at: Time.now, updated_at: Time.now)
-    Invoice.create(status: "paid", merchant: merchant_2, customer: customer_2,created_at: Time.now, updated_at: Time.now)
-    Invoice.create(status: "paid", merchant: merchant_3, customer: customer_3,created_at: Time.now, updated_at: Time.now)
+    Invoice.create(status: "paid", merchant_id: merchant_1.id, customer_id: customer_1.id,created_at: Time.now, updated_at: Time.now)
+    Invoice.create(status: "paid", merchant_id: merchant_2.id, customer_id: customer_2.id,created_at: Time.now, updated_at: Time.now)
+    Invoice.create(status: "paid", merchant_id: merchant_3.id, customer_id: customer_3.id,created_at: Time.now, updated_at: Time.now)
 
-    get "/api/v1/invoices"
+    invoice = Invoice.last
 
-    invoices = JSON.parse(response.body)
+    get "/api/v1/invoices/#{invoice.id}"
+
+    served_invoice = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-    expect(invoices.count).to eq(3)
+    expect(served_invoice[:merchant_id]).to eq(invoice.merchant_id)
 
   end
 
