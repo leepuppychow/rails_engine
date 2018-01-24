5c453521de59c8f638cae6fae1e405a6484bcb3b
BLaurenB <blbillington1@gmail.com>
2018-01-23 21:12:35 -0700

    working on BI for personB

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,8 +1,10 @@
 class Merchant < ApplicationRecord
   validates :name, :created_at, :updated_at, presence: true
   has_many :invoices
+  has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
+  
   has_many :items
-  has_many :invoice_items, through: :items 
+  has_many :invoice_items, through: :items
 
 end
