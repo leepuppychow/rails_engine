18e5d5eb9ddf40cfa1e01139e6cdf660a3a19d44
BLaurenB <blbillington1@gmail.com>
2018-01-24 08:31:06 -0700

    revenue by date might be working

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,6 +3,11 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
   has_many :items
-  has_many :invoice_items, through: :items 
+  has_many :invoice_items, through: :items
+
+
+  def revenue_by_date(date)
+    Merchant.joins(:invoice_items).where(invoice_items: {updated_at: date}).sum("invoice_items.quantity * invoice_items.unit_price")
+  end
 
 end
