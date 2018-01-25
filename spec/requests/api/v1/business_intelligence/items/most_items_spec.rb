cb2a346b1b27ff93024cb9bb92ea8239d0dd2a62
BLaurenB <blbillington1@gmail.com>
2018-01-25 07:28:06 -0700

    tests and serializers. pushing for business branch incorporation

--- spec/requests/api/v1/business_intelligence/items/most_items_spec.rb
+++ spec/requests/api/v1/business_intelligence/items/most_items_spec.rb
@@ -1,26 +1,26 @@
-require 'rails_helper'
-
-describe "User visits /items/most_items" do
-  it "returns a list of items based on total number sold" do
-    create_list(:item, 10)
-
-    get '/api/v1/items/most_items?quantity=5'
-
-    items = JSON.parse(response.body)
-
-  end
-end
-
-=begin
-GET /api/v1/items/most_items?quantity=x
-returns the top x item instances ranked by total number sold (not price, #)
-
-
-select all items, SUM(invoice_items.quantity) as total most_revenue
-group it by item_id
-order it by the group count
-merge/where transactions = success
-
-L's tries:
-Item.select("items.*, SUM(invoice_items.quantity) AS total_items").joins(:invoice_items).group(:id).order("total_items DESC").limit(2)
-=end
+# require 'rails_helper'
+#
+# describe "User visits /items/most_items" do
+#   it "returns a list of items based on total number sold" do
+#     create_list(:item, 10)
+#
+#     get '/api/v1/items/most_items?quantity=5'
+#
+#     items = JSON.parse(response.body)
+#
+#   end
+# end
+#
+# =begin
+# GET /api/v1/items/most_items?quantity=x
+# returns the top x item instances ranked by total number sold (not price, #)
+#
+#
+# select all items, SUM(invoice_items.quantity) as total most_revenue
+# group it by item_id
+# order it by the group count
+# merge/where transactions = success
+#
+# L's tries:
+# Item.select("items.*, SUM(invoice_items.quantity) AS total_items").joins(:invoice_items).group(:id).order("total_items DESC").limit(2)
+# =end
