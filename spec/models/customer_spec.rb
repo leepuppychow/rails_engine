f10f39f1ab612036baaa8417f5dcd041bad0f5eb
leepuppychow <leepuppychow@gmail.com>
2018-01-25 18:55:47 -0700

    working on 3 failing tests in spec harness

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -13,15 +13,4 @@ describe Customer do
     it {is_expected.to have_many(:merchants).through(:invoices)}
   end
 
-  describe "Instance Methods" do
-    xit "can find favorite customer" do
-      merchant = create(:merchant)
-      create_list(:customer, 4)
-      custom_customer = create(:customer)
-      create_list(:invoice, 3, customer: custom_customer, merchant: merchant)
-      create(:invoice, merchant: merchant)
-
-      expect(merchant.favorite_customer).to eq custom_customer
-    end
-  end
 end
